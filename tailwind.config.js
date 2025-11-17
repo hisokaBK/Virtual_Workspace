ae4fff231c0ae64af95761eba50acf4ed6ac2181
bilal <bilalbakessou@gmail.com>
2025-11-17 12:12:26 +0100

    project structur

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Jeux_Video_Catalogue/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    theme: {
+  screens: {
+      'xsm':'600px',
+  }},
+  },
+  plugins: [],
+}
+
